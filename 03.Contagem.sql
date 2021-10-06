12b7f751d083b6623fcda6a2400b5b6e8f83e37d
Nelson Rafael Padilha <nelson.padilha@thomsonreuters.com>
2021-10-06 12:14:04 -0300

    Ajustando query de count para QTDE_DFE_LOB_LIMPEZA

--- 03.Contagem.sql
+++ 03.Contagem.sql
@@ -11,9 +11,9 @@ declare
 	QTDE_DFE_LOB_LIMPEZA integer;
 	QTDE_DFE_LOB integer;
 begin   
-	select count(id_lob) INTO QTDE_LIMPEZA_LOBS_ANTES from msaf_dfe.limpeza_lobs;
-	select count(id)  INTO QTDE_DFE_LOB_LIMPEZA from msaf_dfe.dfe_lob where id not in (select distinct(id_lob) from msaf_dfe.limpeza_lobs);
+	select count(*) INTO QTDE_LIMPEZA_LOBS_ANTES from msaf_dfe.limpeza_lobs;
+	select count(a.id) INTO QTDE_DFE_LOB_LIMPEZA from msaf_dfe.dfe_lob a left join msaf_dfe.limpeza_lobs b on a.id = b.id_lob where b.id_lob is null;
 	select count(id)  INTO QTDE_DFE_LOB from msaf_dfe.dfe_lob;
 	raise notice '% as qtde_limpeza_lobs_antes, % as QTDE_DFE_LOB_LIMPEZA, % as QTDE_DFE_LOB', QTDE_LIMPEZA_LOBS_ANTES, QTDE_DFE_LOB_LIMPEZA, QTDE_DFE_LOB;
 end; 
-$$
+$$ 
